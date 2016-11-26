ee6625aaba3eb83c3429b09f136eab0025bc1687
Arun Karunagath (kra3) <the1.arun@gmail.com>
2016-11-26 20:18:10 +0530

    Integrated navigation panel as a react component

---
+++ app/scripts/components/navigation.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+let buildNavItems = (times) => [...Array(times)].map((num, i) =>
+  <li className="pure-menu-item" key={i+1}>
+    <Link className="pure-menu-link" to="home">
+      <img src="images/nav.png" className="pure-img" />
+      <span>Nav {i + 1}</span>
+    </Link>
+  </li>
+)
+
+const Navigation = (props) => {
+  return (
+    <div className="sidebar pure-u-1 pure-u-md-2-24">
+      <div className="header">
+        <h1 hidden>Artikule</h1>
+        <Link to="home">
+          <img src="images/logo.png" className="pure-img"/>
+        </Link>
+
+        <div className="pure-g">
+          <div className="pure-button pure-u-1-2">
+            <i className="fa fa-bars"></i>
+          </div>
+          <div className="pure-button pure-u-1-2">
+            <i className="fa fa-search"></i>
+          </div>
+        </div>
+
+        <nav className="pure-menu">
+          <ul className="pure-menu-list">
+            {buildNavItems(4)}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
+};
+
+export default Navigation;
--- app/scripts/pages/app.jsx
+++ app/scripts/pages/app.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from '../components/header.jsx'
 import Footer from '../components/footer.jsx'
+import Navigation from '../components/navigation.jsx'
 
 const App = (props) => {
     return (
-      <div>
-        <Header />
-        <div className="content">
+      <div className="pure-g">
+        <Navigation />
+        <div className="content pure-u-1 pure-u-md-22-24">
           {props.children}
         </div>
-          <Footer />
       </div>
     );
 };
